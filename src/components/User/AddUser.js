b7d5459891388520e34559a3066f6525055aad2c
dhruv481 <dguptadg01@gmail.com>
2022-11-15 01:19:07 +0530

    adding wrapper component

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -12,17 +13,17 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-        setError({
-            title: "Invalid Input",
-            message: "Please enter a valid name and age (non-empty values). "
-        });
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid name and age (non-empty values). ",
+      });
       return;
     }
     if (+enteredAge < 1) {
-        setError({
-            title: "Invalid Age",
-            message: "Please enter a valid age (> 0). "
-        });
+      setError({
+        title: "Invalid Age",
+        message: "Please enter a valid age (> 0). ",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -43,8 +44,14 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -64,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
